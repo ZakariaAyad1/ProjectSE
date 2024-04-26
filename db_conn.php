8b3fc33f1c31bc450c01b4dee8e178e6403fcd0c
Md123etplus <moussadembel976@gmail.com>
2024-04-26 14:26:09 +0100

    msg

--- db_conn.php
+++ db_conn.php
@@ -4,8 +4,8 @@ $sName = "localhost";
 $uName = "root";
 $pass = "";
 $db_name = "todolist";
-$port="3307";
+$port="3308";
 
-  $conn= new mysqli($sName,$uName,$pass,$db_name,$port);
+  $conn= mysqli_connect($sName,$uName,$pass,$db_name,$port);
 
 ?>
